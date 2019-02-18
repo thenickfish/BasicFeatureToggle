8c6002249025c8911fcda10b94b2430957232a6f
Nick Fish <thenickfish@gmail.com>
2019-02-18 10:23:33 -0700

    add tests to build

--- build.ps1
+++ build.ps1
@@ -1,7 +1,20 @@
+function Exec {
+    [CmdletBinding()]
+    param(
+        [Parameter(Position = 0, Mandatory = 1)][scriptblock]$cmd,
+        [Parameter(Position = 1, Mandatory = 0)][string]$errorMessage = ($msgs.error_bad_command -f $cmd)
+    )
+    & $cmd
+    if ($lastexitcode -ne 0) {
+        throw ("Exec: " + $errorMessage)
+    }
+}
+
 Push-Location src
 try {
-    dotnet restore
-    dotnet build
+    exec { & dotnet restore -v q }
+    exec { & dotnet build -v q }
+    exec { & dotnet test --test-adapter-path:. --logger:Appveyor }
     # msbuild /v:q /m BasicFeatureToggle.sln
 }
 finally {
